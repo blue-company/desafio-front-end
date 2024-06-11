2ef347742c57728cc39da513cee1aaa3c4e92057
joaokeuwe <joaov2872@hotmail.com>
2024-06-11 19:46:19 -0300

    feat: estilização da navbar e ajustes

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -1,15 +1,63 @@
-import React from 'react'
-import AppBar from "@mui/material/AppBar";
-import Typography from '@mui/material/Typography'
+import React from "react";
+import Typography from "@mui/material/Typography";
+import { FaSearch } from "react-icons/fa";
+import { CiBellOn } from "react-icons/ci";
+import { IoIosArrowDown } from "react-icons/io";
+import Image from "next/image";
+import USA from "@/assets/icons/usa.png";
+import profile from "@/assets/images/profile.jpg";
 
 export const NavBar = () => {
   return (
     <div>
-      <AppBar position="static" className='h-20 bg-[#fff] shadow-md'>
-        <Typography variant="h5" className='text-3xl text-black p-5 font-bold'>dashboard</Typography>
-      </AppBar>
+      <div className="h-24 bg-[#fff] shadow-md flex items-center justify-around">
+        <Typography variant="h5" className="text-3xl text-black p-5 font-bold">
+          dashboard
+        </Typography>
+        <div>
+          <div className="relative flex items-center">
+            <input
+              type="text"
+              placeholder="Pesquisar informações"
+              className="
+          bg-[#f3f3f3]
+          rounded-xl
+          text-[#c0c0c0]
+          h-12
+          pl-10
+          pr-5
+          w-96
+        "
+            />
+            <FaSearch className="text-[#2c02ff] absolute left-3 top-1/2 transform -translate-y-1/2" />
+          </div>
+        </div>
+
+        <div className="flex gap-3 items-center">
+          <Image src={USA} alt="usa logo" width={30} />
+          <p className="font-bold text-[14px]">PT (BR)</p>
+          <IoIosArrowDown />
+        </div>
+
+        <div className="flex gap-4 items-center">
+          <div className="bg-[#ffcba075] rounded-xl h-10">
+            <CiBellOn className="text-[35px]  text-[#af570f]" />
+          </div>
+          <Image
+            src={profile}
+            alt="profile image"
+            width={50}
+            className="rounded-xl"
+          />
+          <span>
+            <p>João Keuwe</p>
+            Comercial
+          </span>
+          <IoIosArrowDown />
+        </div>
+      </div>
     </div>
   );
-}
+};
 
-export default NavBar
+export default NavBar;
--- src/components/sideMenu/index.tsx
+++ src/components/sideMenu/index.tsx
@@ -1,7 +1,11 @@
 "use client";
 import Image from "next/image";
 import React, { useState } from "react";
-import logo from "../../assets/images/logo.svg";
+import logo from "@/assets/icons/logoWhite.png";
+import know from "@/assets/icons/know.png";
+import DashBoard from "@/assets/icons/dashIcon.png";
+import { RiBarChartLine } from "react-icons/ri";
+import { Button } from "@mui/material";
 
 const SideBar = () => {
   const [openMenu, setOpenMenu] = useState(true);
@@ -16,21 +20,38 @@ const SideBar = () => {
       </button>
 
       {openMenu && (
-        <div className="bg-white w-72 h-[100vh] shadow-xl flex flex-col items-center mt-7 ">
-          <main className="flex gap-5 mb-10  items-center">
-            <span className="bg-purple-500 p-2 rounded-xl ">
-              <Image src={logo} alt="" width={50} height={50} />
+        <div className="bg-white w-80 h-[100vh] shadow-xl flex flex-col items-center pt-7 ">
+          <main className="flex gap-5 mb-10 items-center w-[100%] ml-20">
+            <span className="bg-[#2500FF] p-2 rounded-xl">
+              <Image src={logo} alt="" width={40} height={50} />
             </span>
             <p className="text-3xl font-bold">Blue</p>
           </main>
+
+          {/* componentizar */}
           <ul className="space-y-4">
-            <li className="hover:bg-gray-700 rounded-2xl bg-[#2500FF] text-white p-3 w-60 text-center text-xl">
+            <li className="hover:bg-gray-700 rounded-2xl bg-[#2500FF] text-[20px] text-white p-3 w-64 text-center text-xl flex items-center gap-7">
+              <Image src={DashBoard} width={30} height={30} alt="" />
               Dashboard
             </li>
-            <li className="hover:bg-gray-700 rounded-2xl bg-[#2500FF] text-white p-3 w-60 text-center text-xl">
+
+            <li className="hover:bg-gray-700 rounded-2xl bg-[#2500FF] text-[20px] text-white p-3 w-64 text-center text-xl flex items-center gap-7">
+              <RiBarChartLine className="text-xl" />
               LeaderBoard
             </li>
           </ul>
+
+          <div className="bg-[#2500FF] w-[70%] h-[290px] flex flex-col items-center px-4 pt-2 pb-2 mt-[40vh] rounded-2xl text-white justify-around">
+            <Image src={know} alt="" width={50} />
+            <h2 className="text-xl text-center font-bold ">
+              Conheça a <br /> blue
+            </h2>
+            <p className="text-sm text-center ">
+              Saiba mais informações sobre a Blue, uma operadora de saúde Health
+              Tech
+            </p>
+            <Button className="bg-white text-[#2500FF] rounded-md font-bold w-[70%] h-8">conhecer</Button>
+          </div>
         </div>
       )}
     </div>
