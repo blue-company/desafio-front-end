a4192d8e29d49f3f8cd5be286cf0536fef091f36
João Phillipe Fernandes <joao.phillipe@ufba.br>
2024-06-12 18:25:18 -0300

    Initial MUI config

---
+++ src/themes/Theme.ts
@@ -0,0 +1,22 @@
+"use client";
+import { createTheme } from "@mui/material";
+
+export const theme = createTheme({
+    palette: {
+        primary: {
+            main: "#2500FF",
+            dark: "#1900b2",
+            light: "#5033ff",
+            contrastText: "#ffffff",
+        },
+
+        background: {
+            default: "#f7f6f3",
+            paper: "#ffffff",
+
+
+        },
+
+    },
+
+});
