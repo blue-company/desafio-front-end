e19e5cb329dc54df42432a70389d010d6fb5a6e7
Lucas Campos <lucasoliveiracampos81@gmail.com>
2024-06-13 11:22:44 -0300

    refactor: implementing sells-chart to reduce code

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,7 @@
 import "bootstrap/dist/css/bootstrap.min.css";
-import {
-  BarChart,
-  FileSpreadsheet,
-  Pencil,
-  Upload,
-  UserPlus,
-} from "lucide-react";
 import DashboardCard from "./components/dashboard/card/card";
 import Header from "./components/header/header";
 import Sidebar from "./components/sidebar/sidebar";
-import NewsMiniCard from "./components/dashboard/card/news-mini-card";
 import ChartCard from "./components/dashboard/card/charts/platforms-chart";
 import IncomesChart from "./components/dashboard/card/charts/incoming-chart";
 import LifesChart from "./components/dashboard/card/charts/lifes-chart";
@@ -17,13 +9,14 @@ import RealityChart from "./components/dashboard/card/charts/reality-chart";
 import CountryChart from "./components/dashboard/card/charts/country-chart";
 import ServiceChart from "./components/dashboard/card/charts/service-chart";
 import ProductsChart from "./components/dashboard/card/charts/products-chart";
+import SellsChart from "./components/dashboard/card/charts/sells-chart";
 
 function App() {
   return (
-    <div className="md:grid h-screen md:grid-cols-[14rem_1fr] flex flex-col ">
-      <div className="flex flex-col lg:flex-row w-screen">
+    <div className="lg:grid h-screen lg:grid-cols-[14rem_1fr] flex flex-col ">
+      <div className="flex flex-col xl:flex-row w-screen">
         <Sidebar />
-        <div className="flex w-screen flex-col">
+        <div className="flex  flex-col">
           <Header />
           <main className="flex p-5 flex-col gap-2 ">
             <div className="flex xl:flex-row flex-col gap-4">
@@ -31,55 +24,7 @@ function App() {
                 title="Vendas hoje"
                 className="xl:w-[40vw] xl:h-[30vh] h-fit"
               >
-                <div className="flex justify-between">
-                  <span className="text-gray-400 font-semibold">
-                    Resumo de vendas
-                  </span>
-
-                  <button className="flex border font-semibold rounded-lg border-blue-950/20 p-1 gap-1 text-sm items-center text-blue-950">
-                    <Upload size={20} />
-                    Exportar
-                  </button>
-                </div>
-
-                <div className="flex xl:flex-row flex-col items-center  gap-2 justify-around mt-10 mb-5">
-                  <NewsMiniCard
-                    icon={
-                      <BarChart className="bg-pink-700 w-9 h-9 p-1 rounded-full" />
-                    }
-                    results={"R$ 1,000"}
-                    description="Total de vendas"
-                    percentageText="8% a mais que ontem"
-                    className="bg-red-500/25"
-                  />
-                  <NewsMiniCard
-                    icon={
-                      <FileSpreadsheet className="bg-orange-400 w-9 h-9 p-1 rounded-full" />
-                    }
-                    results={300}
-                    description="Total de contratos"
-                    percentageText="5% a mais que ontem"
-                    className="bg-yellow-500/25"
-                  />
-                  <NewsMiniCard
-                    icon={
-                      <Pencil className="bg-green-500 w-9 h-9 p-1 rounded-full" />
-                    }
-                    results={5}
-                    description="Implantadas"
-                    percentageText="1,2% a mais que ontem"
-                    className="bg-green-500/25"
-                  />
-                  <NewsMiniCard
-                    icon={
-                      <UserPlus className="bg-purple-500 w-9 h-9 p-1 rounded-full" />
-                    }
-                    results={9}
-                    description="Novos contratos"
-                    percentageText="0,5% a mais que ontem"
-                    className="bg-purple-500/25"
-                  />
-                </div>
+                <SellsChart />
               </DashboardCard>
 
               <DashboardCard
@@ -89,7 +34,7 @@ function App() {
                 <ChartCard />
               </DashboardCard>
             </div>
-            <div className="lg:grid lg:grid-cols-[40%_27%_27%] flex flex-col lg:gap-8 gap-4">
+            <div className="xl:grid xl:grid-cols-[40%_27%_27%] flex flex-col xl:gap-8 gap-4">
               <DashboardCard title="Rendimento total" className="h-[30vh]">
                 <IncomesChart />
               </DashboardCard>
