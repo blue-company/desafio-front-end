60fa71f7597b72260bf8be918c31d1008c709f37
joaokeuwe <joaov2872@hotmail.com>
2024-06-13 13:37:28 -0300

    feat: finalizando a estilização do gráfico lifes charts

--- src/components/charts/lifesCharts.tsx
+++ src/components/charts/lifesCharts.tsx
@@ -1,12 +1,14 @@
 import React, { useEffect, useRef } from "react";
 import ApexCharts from "apexcharts";
+import Image from "next/image";
+import IconBlue from "@/assets/icons/iconBlue.png";
+import IconGreen from "@/assets/icons/iconGreen.png";
 
-const PlataformChart: React.FC = () => {
+const LifesCharts: React.FC = () => {
   const chartRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
     const options = {
-      colors: ["#0084ff", "#00f43d"],
       series: [
         {
           name: "TD Consultoria",
@@ -17,8 +19,11 @@ const PlataformChart: React.FC = () => {
           data: [35, 45, 45, 60, 50, 52, 45],
         },
       ],
+      legend: {
+        show: false,
+      },
       chart: {
-        height: 300,
+        height: 250,
         type: "area",
         toolbar: {
           show: false,
@@ -62,7 +67,37 @@ const PlataformChart: React.FC = () => {
     };
   }, []);
 
-  return <div id="chart" ref={chartRef}></div>;
+  return (
+    <div>
+      <div>
+        <div>
+          <div id="chart" ref={chartRef}></div>
+          <span className="flex justify-center">
+            <hr className="h-[0.8px] w-[90%] bg-[#ddd] mt-[-1rem]" />
+          </span>
+          <div className="gap-3 flex mt-[-1rem] items-center justify-center ">
+            <div className="flex items-center flex-col">
+              <div className="flex items-center gap-4 mt-1">
+                <Image width={30} src={IconBlue} alt="Icon Blue" />
+                Utimo mês
+              </div>
+              <span className="">4.520</span>
+            </div>
+
+            <hr className="h-5 w-[0.10px] bg-[#DDD] mb-4" />
+
+            <div className="flex items-center flex-col">
+              <div className="flex items-center gap-4 mt-1">
+                <Image width={30} src={IconGreen} alt="Icon Blue" />
+                Este mês
+              </div>
+              <span>3.452</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
-export default PlataformChart;
+export default LifesCharts;
