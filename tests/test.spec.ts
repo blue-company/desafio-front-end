8cf41f606aa8304bc6397d2a1fdd86d0bfaf32d9
João Phillipe Fernandes <joao.phillipe@ufba.br>
2024-06-17 08:21:51 -0300

    fix tests url

--- tests/test.spec.ts
+++ tests/test.spec.ts
@@ -2,7 +2,7 @@ import { test, expect } from '@playwright/test';
 
 test('Verificar modo escuro', async ({ page }) => {
 
-    await page.goto('http://localhost:3000');
+    await page.goto('http://desafiofrontendblue.vercel.app/');
 
 
     const initialThemeColor = await page.$eval('header', el => {
--- tests/translationTest.spec.ts
+++ tests/translationTest.spec.ts
@@ -2,7 +2,7 @@ import { test, expect } from '@playwright/test';
 
 
 test('Translation Test', async ({ page }) => {
-    await page.goto('http://localhost:3000');
+    await page.goto('http://desafiofrontendblue.vercel.app/');
 
 
     const initialTextElement = await page.getByRole('heading', { name: 'Vendas Hoje' });
