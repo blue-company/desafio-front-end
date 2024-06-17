98075d0da15ae37ebe8f4a2ae686d2ff44ee3392
João Phillipe Fernandes <joao.phillipe@ufba.br>
2024-06-17 02:44:26 -0300

    fix-i18n

--- src/locale/i18n.ts
+++ src/locale/i18n.ts
@@ -1,27 +1,31 @@
-"use client"
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import enTranslations from './en.json';
 import ptTranslations from './pt.json';
 import esTranslations from './es.json';
 
-i18n.use(initReactI18next).init({
-    resources: {
-        en: {
-            ...enTranslations
+// Inicialização do i18n
+const initializeI18n = () => {
+    i18n.use(initReactI18next).init({
+        resources: {
+            en: {
+                ...enTranslations
+            },
+            pt: {
+                ...ptTranslations
+            },
+            es: {
+                ...esTranslations
+            }
         },
-        pt: {
-            ...ptTranslations
-        },
-        es: {
-            ...esTranslations
+        lng: typeof window !== 'undefined' ? localStorage.getItem('selectedLanguage') || "pt" : "pt",
+        fallbackLng: "en",
+        interpolation: {
+            escapeValue: false
         }
-    },
-    lng: "pt",
-    fallbackLng: "en",
-    interpolation: {
-        escapeValue: false
-    }
-});
+    });
+};
+
+initializeI18n();
 
 export default i18n;
